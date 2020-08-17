2e02cd2868c9ff03f9fc21549c75865d8bcc5395
U-Zo <kwoojo0127@naver.com>
2020-08-17 19:16:30 +0900

    Add java 2193 for dynamic programming

---
+++ baekjoon/dynamic_programming/2193_이친수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[n + 2];
+
+        // 1 - 1
+        // 2 - 10
+        // 3 - 100 101
+        // 4 - 1000 1001 1010
+        // 5 - 10001 10000 10010 10100 10101
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[n]);
+    }
+}
