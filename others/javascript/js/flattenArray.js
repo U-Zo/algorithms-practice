a1bb6f537d4fb71631e5556474a8d23a0774a509
Woojo Kim <kwoojo0127@naver.com>
2020-11-23 22:37:56 +0900

    Update flattenArray

--- others/javascript/js/flattenArray.js
+++ others/javascript/js/flattenArray.js
@@ -1,10 +1,9 @@
-const flattenArr = (arr) => {
-  return arr.reduce(
+const flattenArr = (arr) =>
+  arr.reduce(
     (prev, curr) =>
       curr instanceof Array ? [...prev, ...flattenArr(curr)] : [...prev, curr],
     []
   );
-};
 
 const output = flattenArr([[2, [[3]]], 4, [[[5]]]]);
 console.log(output);
