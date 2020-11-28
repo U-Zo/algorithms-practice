992f904ff4144f3a055d905379b386d4c76ec9a1
Woojo Kim <kwoojo0127@naver.com>
2020-11-28 17:53:58 +0900

    Add solution of programmers level1 완주하지 못한 선수

---
+++ programmers/level1/완주하지_못한_선수.js
@@ -0,0 +1,10 @@
+function solution(participant, completion) {
+  participant.sort();
+  completion.sort();
+
+  for (const idx in participant) {
+    if (participant[idx] !== completion[idx]) {
+      return participant[idx];
+    }
+  }
+}
